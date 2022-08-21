7b7a2072998698518c8b58603abe6cc86c529037
AryanBagheri <95540857+AryanBagheri@users.noreply.github.com>
2022-08-21 16:57:00 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace cSharp8
+{
+    public interface Building
+    {
+        public string Owner { get; set; }
+    }
+    public class House : Building
+    {
+        public string Owner { get; set; }
+    }
+    public class Apartment : Building
+    {
+        public string Owner { get; set; }
+    }
+    public abstract class Builder
+    {
+        public abstract Building BuildingFactory();
+        public Building Result(string name)
+        {
+            var building = BuildingFactory();
+            building.Owner = name;
+            return building;
+        }
+    }
+    public class HouseBuilder : Builder
+    {
+        public override Building BuildingFactory()
+        {
+            return new House();
+        }
+    }
+    public class ApartmentBuilder : Builder
+    {
+        public override Building BuildingFactory()
+        {
+            return new Apartment();
+        }
+    }
+    public class Facade
+    {
+        public Building Pick(Builder pick, string name)
+        {
+            return pick.Result(name);   
+        }
+        public string Name(Building pick)
+        {
+            return pick.Owner;   
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var prog = new Facade();
+            Console.WriteLine("pls enter your name");
+            string name = Console.ReadLine();
+            Building yourBuilding = prog.Pick(new ApartmentBuilder(), name);
+            string owner = prog.Name(yourBuilding);
+            Console.WriteLine(owner);
+        }
+    }
+}
